76c50851f699ed2499fba4c7b07177655325bdcb
LuciferRJ28 <jimishravat@hotmail.com>
2023-02-08 23:10:49 +0530

    admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./helper/sidebar.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+
     <title>Document</title>
 </head>
+
 <body>
-    This is admin dashboard
-    <a href="../index.php"> Home </a>
+    <?php include("./helper/sidebar.php") ?>
+    <main>
+        <h1>My Dashboard</h1>
+        <p class="text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur animi voluptatibus cum maxime distinctio
+            iste quod deleniti eius, autem voluptates cumque suscipit iure quasi eligendi ullam. Sapiente eligendi porro
+            reprehenderit corrupti error facilis quo, fugiat fugit? Maiores aliquam ad, molestiae iste nihil, commodi
+            doloremque tempore excepturi aut id ducimus unde?
+        </p>
+        <p class="copyright">
+            &copy; 2023 - <span>Jimish Ravat</span> All Rights Reserved.
+        </p>
+    </main>
+
+    <script src="./helper/sidebar.js"></script>
 </body>
+
 </html>
