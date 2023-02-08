2d3a65b9a45f741b9499cd897c1c15f03fa99429
LuciferRJ28 <jimishravat@hotmail.com>
2023-02-08 20:33:50 +0530

    login

--- login.php
+++ login.php
@@ -17,15 +17,15 @@
             <form action="./checkLogin.php" method="POST">
                 <div class="row">
                     <i class="fas fa-user"></i>
-                    <input type="text" placeholder="Email or Phone" required>
+                    <input type="text" name="username" placeholder="Username" required>
                 </div>
                 <div class="row">
                     <i class="fas fa-lock"></i>
-                    <input type="password" placeholder="Password" required>
+                    <input type="password" name="password" placeholder="Password" required>
                 </div>
                 <div class="pass"><a href="./forgotPassword.php">Forgot password?</a></div>
                 <div class="row button">
-                    <input type="submit" value="Login">
+                    <input type="submit" name="login" value="Login">
                 </div>
                 <div class="signup-link">Not a member? <a href="./signup.php">Signup now</a></div>
             </form>
